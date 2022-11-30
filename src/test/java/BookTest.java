51ab7bda9239a8c892dc9161d1df362f2681e893
Shashikant-15 <shashikant.tanti@knoldus.com>
2022-11-30 20:55:55 +0530

    initial commit

---
+++ src/test/java/BookTest.java
@@ -0,0 +1,19 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.knoldus.junit.test.config.DataConfig;
+import org.knoldus.junit.test.config.WebConfig;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@WebAppConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { WebConfig.class, DataConfig.class })
+public class BookTest {
+
+    @Test
+    public void testSayHello() {
+        Assert.assertEquals("Hello world!", "Hello world!");
+    }
+}
